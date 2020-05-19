f41224af81c3e764556d951ef740ee80bf842554
Huang DiaoZhong <VPHT204@globalymc.com>
2020-05-19 14:04:27 +0800

    SpringBoot常用定时任务配置实战

--- src/main/java/net/xdclass/base_project/task/TestTask.java
+++ src/main/java/net/xdclass/base_project/task/TestTask.java
@@ -16,12 +16,34 @@ import java.util.Date;
 public class TestTask {
 
     /**
-    * @author hdz
-    * @Description 两秒执行一次Sum方法，打印当前系统时间
-    * @Date 13:47 2020/05/19
-    **/
-    @Scheduled(fixedRate = 2000)
+     * @author hdz
+     * @Description 两秒执行一次Sum方法，打印当前系统时间
+     * @Date 13:47 2020/05/19
+     **/
+    /*@Scheduled(fixedRate = 2000)
     public void sum() {
         System.out.println("当前时间："+new Date());
+    }*/
+
+    /**
+     * @author hdz
+     * @Description 两秒执行一次Sum2方法，打印当前系统时间
+     * @Date 13:52 2020/05/19
+     **/
+    /*@Scheduled(fixedRateString="2000")
+    public void sum2() throws InterruptedException{
+
+        Thread.sleep(4000L);
+        System.out.println("结束 当前时间:"+new Date());
+    }*/
+
+    /**
+     * @author hdz
+     * @Description cron一秒执行一次Sum3方法，打印当前系统时间
+     * @Date 13:52 2020/05/19
+     **/
+    //@Scheduled(cron="*/1 * * * * *")
+    public void sum3(){
+        System.out.println("cron 每秒 当前时间:"+new Date());
     }
 }
